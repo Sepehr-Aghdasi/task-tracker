d4b01e3c3013a99fdb1d0b5ccdc1be039f00ed8f
Sepehr-Aghdasi82 <sepehr.aghdasi82@gmail.com>
2022-08-01 14:32:25 +0430

    Add style to button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() backgroundColor: string = '';
+  @Input() text: string = '';
+  @Output() btnClick = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleClick() {
+    this.btnClick.emit();
   }
-
 }
