80759c37518f8d9537c143fbea557c814bcc1fd5
Sepehr-Aghdasi82 <sepehr.aghdasi82@gmail.com>
2022-08-02 16:35:12 +0430

    Create addTask function

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { TasksService } from './../../services/tasks.service';
+import { ITasks } from './../../../interfaces/Task';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.scss']
+  styleUrls: ['./add-task.component.scss'],
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<ITasks> = new EventEmitter();
 
-  constructor() { }
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private tasksService: TasksService) {}
+
+  ngOnInit(): void {}
+
+  addTask() {
+    if (!this.text && !this.day) {
+      console.log('empty input');
+      return;
+    }
+    this.onAddTask.emit();
+    console.log(this.text, this.day, this.reminder);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
   }
-
 }
