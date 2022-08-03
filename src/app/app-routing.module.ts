7c581a8ab3059e550e20b4a1539f2ca997eef6a8
Sepehr-Aghdasi82 <sepehr.aghdasi82@gmail.com>
2022-08-03 15:44:22 +0430

    Add task & about-us rout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TasksComponent } from './components/tasks/tasks.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+const routes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
