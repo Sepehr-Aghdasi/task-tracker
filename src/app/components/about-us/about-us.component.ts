1e8e7f4be1d0850fff5e474b9083224e72af50c4
Sepehr-Aghdasi82 <sepehr.aghdasi82@gmail.com>
2022-08-03 15:49:52 +0430

    Create variable for background of button

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  styleUrls: ['./about-us.component.scss'],
 })
 export class AboutUsComponent implements OnInit {
+  btnBackgroundColor: string = 'green';
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
